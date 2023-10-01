b16457c8ef8532f7fa6bc3ad4b202b9b3c57f044
haramchoi <haramchoi@haramchois-MacBook-Air.local>
2023-10-01 17:10:39 +0200

    first commit

---
+++ test/helper/jest.setup.ts
@@ -0,0 +1,52 @@
+import { UiControlClient, UiController } from 'askui';
+import { AskUIAllureStepReporter } from '@askui/askui-reporters';
+
+// Server for controlling the operating system
+let uiController: UiController;
+
+// Client is necessary to use the askui API
+// eslint-disable-next-line import/no-mutable-exports
+let aui: UiControlClient;
+
+jest.setTimeout(60 * 1000 * 60);
+
+beforeAll(async () => {
+  uiController = new UiController({
+    /**
+     * Select the display you want to run your tests on, display 0 is your main display;
+     * ignore if you have only one display
+     */
+    display: 0,
+  });
+
+  await uiController.start();
+
+  aui = await UiControlClient.build({
+/*    credentials: {
+      workspaceId: '<your workspace id>',
+      token: '<your access token>',
+    },
+ */
+    reporter: new AskUIAllureStepReporter(),
+  });
+
+  await aui.connect();
+});
+
+beforeEach(async () => {
+  await aui.startVideoRecording();
+});
+
+afterEach(async () => {
+  await aui.stopVideoRecording();
+  const video = await aui.readVideoRecording();
+  AskUIAllureStepReporter.attachVideo(video);
+});
+
+afterAll(async () => {
+  aui.disconnect();
+
+  await uiController.stop();
+});
+
+export { aui };
---
+++ test/my-first-askui-test-suite.test.ts
@@ -0,0 +1,19 @@
+import { aui } from './helper/jest.setup';
+
+describe('jest with askui', () => {
+  it('should generate an (interactive) annotation', async () => {
+    // For Windows users:
+    // Use annotate() to create an annotated HTML file
+    // of your screen that is saved under <project_root>/report
+    await aui.annotate();
+
+    // Uncomment for macOS and Linux
+    // Delete the lines above to not trigger annotate()
+    // await aui.annotateInteractively();
+  });
+
+  it('should click on my element', async () => {
+    await aui
+      .moveMouse(600,600).exec();
+  });
+});
